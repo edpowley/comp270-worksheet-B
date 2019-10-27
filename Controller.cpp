6fe970e3b446b737b57a8ea76fe6452de6b2979f
Ed Powley <edpowley@gmail.com>
2019-10-27 20:39:47 +0000

    Worksheet solution

--- Controller.cpp
+++ Controller.cpp
@@ -9,8 +9,30 @@
 //   wind: the acceleration due to wind in pixels/second^2 (positive is rightwards)
 float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enemyPos, float shotAngleRadians, float gravity, float wind)
 {
-	// TODO: calculate the required shot speed (in pixels per second) and return it
-	return 700;
+    Vector2 s = enemyPos - tankPos;
+    
+	// Task 1
+    if (!c_canHandleHeightDifference && !c_canHandleWind)
+    {
+        return sqrtf(gravity * s.x / sinf(2 * shotAngleRadians));
+    }
+    
+    // Task 2
+    else if (c_canHandleHeightDifference && !c_canHandleWind)
+    {
+        return sqrtf(gravity * s.x * s.x
+                     / (s.x * sinf(2*shotAngleRadians) + s.y * (cosf(2*shotAngleRadians) + 1)));
+    }
+    
+    // Task 3
+    else if (c_canHandleWind)
+    {
+        float alpha = (gravity * s.x - wind * s.y) / (gravity * cosf(shotAngleRadians) + wind * sinf(shotAngleRadians));
+        return sqrtf(gravity * alpha * alpha
+                     / (2 * (alpha * sinf(shotAngleRadians) + s.y)));
+    }
+    
+    return 0;
 }
 
 // Calculate the shot angle to hit the target, given the following information.
@@ -21,7 +43,15 @@ float Controller::calculateShotSpeed(const Vector2& tankPos, const Vector2& enem
 //   wind: the acceleration due to wind in pixels/second^2 (positive is rightwards)
 float Controller::calculateShotAngle(const Vector2& tankPos, const Vector2& enemyPos, float shotSpeed, float gravity, float wind)
 {
-	// TODO: calculate the required shot angle (in radians) and return it
-	return M_PI * 0.25f;
+    Vector2 s = enemyPos - tankPos;
+
+    // Task 4
+    float a = gravity * s.x * s.x / (2 * shotSpeed * shotSpeed);
+    float b = s.x;
+    float c = a - s.y;
+    
+    // Can change this sign to get the other solution
+    //                v
+    return -atanf((-b + sqrtf(b*b - 4*a*c)) / (2*a));
 }
 
--- Controller.h
+++ Controller.h
@@ -8,7 +8,7 @@ public:
 	float calculateShotSpeed(const Vector2& tankPos, const Vector2& enemyPos, float shotAngleRadians, float gravity, float wind);
 	float calculateShotAngle(const Vector2& tankPos, const Vector2& enemyPos, float shotSpeed, float gravity, float wind);
 
-	const float c_canHandleHeightDifference = false;
+	const float c_canHandleHeightDifference = true;
 	const float c_canHandleWind = false;
-	const float c_doCalculateAngle = false;
+	const float c_doCalculateAngle = true;
 };
